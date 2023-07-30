3269187babeac97feebc3958b6370284d5b838d9
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2023-07-30 16:29:56 +0530

    Filter Game by Platform

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,16 +1,21 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { BsChevronBarDown } from 'react-icons/bs'
 import usePlatform from '../hooks/usePlatforms'
+import { Platform } from '../hooks/useGames';
 
-function PlatformSelector() {
+interface Props{
+    onSelectPlatform: (platform: Platform) => void;
+    selectedPlatform: Platform | null;
+}
+function PlatformSelector({onSelectPlatform, selectedPlatform}: Props) {
     const {data, error} = usePlatform();
     if(error) return null;
 
   return (
     <Menu>
-        <MenuButton as = {Button} rightIcon={<BsChevronBarDown/>}> Plaform </MenuButton>
+        <MenuButton as = {Button} rightIcon={<BsChevronBarDown/>}> {selectedPlatform? selectedPlatform.name : "Platform" } </MenuButton>
         <MenuList>
-            {data.map(platform => <MenuItem key={platform.id}>{platform.name}</MenuItem>)}
+            {data.map(platform => <MenuItem key={platform.id} onClick={() => onSelectPlatform(platform)}>{platform.name}</MenuItem>)}
         </MenuList>
     </Menu>
   )
