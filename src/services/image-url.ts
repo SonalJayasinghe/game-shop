f4b1ebc71bb0842d1641342bca00f4d579c7bea8
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2023-07-30 17:39:12 +0530

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+
 const getCropedImageUrl = (url:string) => {
-   if(url === null) return '';
+   if(url === null) return noImage;
    const index = url.indexOf('media/') + 'media/'.length; //index of returns upto 'm' in media. the media/ have additional length
    return url.slice(0, index) +  'crop/600/400/' + url.slice(index);
 }
